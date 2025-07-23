fc796eae6aec0893ec0e46bf5a80b13b58e1a6e4
maycon-rodrigues <maycon.zng@gmail.com>
2025-07-23 12:29:47 -0300

    chore: hook to provide balance info

---
+++ app/hooks/useTonBalance.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { TonClient, Address } from '@ton/ton';
+
+export function useTonBalance(address: string | null) {
+  const [balance, setBalance] = useState<number | null>(null);
+
+  const mainnet = 'https://toncenter.com/api/v2/jsonRPC';
+  const testnet = 'https://testnet.toncenter.com/api/v2/jsonRPC';
+
+  useEffect(() => {
+    if (!address) {
+      setBalance(null);
+      return;
+    }
+    const client = new TonClient({ endpoint: testnet });
+    client.getBalance(Address.parse(address))
+      .then(bal => setBalance(Number(bal) / 1e9))
+      .catch(() => setBalance(null));
+  }, [address]);
+
+  return balance;
+}
