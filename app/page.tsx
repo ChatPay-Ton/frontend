1aab8a19a6dd99393cce3e124e6902c96d54b8e8
maycon-rodrigues <maycon.zng@gmail.com>
2025-07-24 16:06:43 -0300

    refactor: change manifestURL

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import AppRouter from './components/AppRouter';
 
 export default function Home() {
   return (
-    <TonConnectUIProvider manifestUrl="https://beige-peaceful-elephant-205.mypinata.cloud/ipfs/bafkreihz4w56ebhzcnbddrtqcgsexffrrvoouv5kratb3dvbrzazdm52a4">
+    <TonConnectUIProvider manifestUrl="https://beige-peaceful-elephant-205.mypinata.cloud/ipfs/bafkreigfhqhpdi3l3ji57txismsaxcdm7wh5b4ov22mpk7rvtkhw7alp64">
       <UserProvider>
         <AppRouter />
       </UserProvider>
